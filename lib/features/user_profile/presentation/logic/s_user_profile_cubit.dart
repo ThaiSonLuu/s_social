24a19ac7aaf99696135a67e93f296384777656db
ThaiSonLuu <sanryoo6@gmail.com>
2024-10-20 16:33:20 +0700

    Build profile user view

---
+++ lib/features/user_profile/presentation/logic/s_user_profile_cubit.dart
@@ -0,0 +1,40 @@
+import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:s_social/core/domain/model/user_model.dart';
+import 'package:s_social/core/domain/repository/user_repository.dart';
+import 'package:s_social/generated/l10n.dart';
+
+part 's_user_profile_state.dart';
+
+class SUserProfileCubit extends Cubit<SUserProfileState> {
+  SUserProfileCubit({
+    required UserRepository userRepository,
+    required String? uid,
+  })  : _userRepository = userRepository,
+        _uid = uid,
+        super(SUserProfileInitial());
+
+  final UserRepository _userRepository;
+  final String? _uid;
+
+  Future<void> getUserInfoByUid() async {
+    try {
+      emit(SUserProfileLoading());
+
+      if (_uid == null) {
+        emit(SUserProfileError(S.current.an_error_occur));
+        return;
+      }
+
+      final user = await _userRepository.getUserById(_uid);
+      if (user == null) {
+        emit(SUserProfileError(S.current.an_error_occur));
+        return;
+      }
+
+      emit(SUserProfileLoaded(user));
+    } catch (e) {
+      emit(SUserProfileError(e.toString()));
+    }
+  }
+}
