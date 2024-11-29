00c38768bdd5ef67e82b14295ddfca0f47e9f402
ThaiSonLuu <sanryoo6@gmail.com>
2024-11-29 10:29:30 +0700

    Refactor home screen

--- lib/features/screen/home/view/home_screen.dart
+++ lib/features/screen/home/view/home_screen.dart
@@ -1,19 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:s_social/core/domain/model/user_model.dart';
+import 'package:s_social/core/presentation/logic/cubit/profile_user/profile_user_cubit.dart';
 import 'package:s_social/core/presentation/view/widgets/text_to_image.dart';
+import 'package:s_social/features/screen/home/logic/post_cubit.dart';
 import 'package:s_social/features/screen/home/view/new_post_screen.dart';
 import 'package:s_social/features/screen/home/view/widget/post_widget.dart';
 import 'package:s_social/core/domain/model/post_model.dart';
-import '../../../../core/presentation/logic/cubit/profile_user/profile_user_cubit.dart';
-import '../../../../generated/l10n.dart';
-import '../logic/post_cubit.dart';
+import 'package:s_social/gen/assets.gen.dart';
+import 'package:s_social/generated/l10n.dart';
 
 class HomeScreen extends StatefulWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+  const HomeScreen({super.key});
 
   @override
-  _HomeScreenState createState() => _HomeScreenState();
+  State<StatefulWidget> createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
@@ -30,130 +31,151 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     final user = context.read<ProfileUserCubit>().currentUser;
+    return Scaffold(
+      appBar: _buildTopAppBar(),
+      body: RefreshIndicator(
+        onRefresh: () => _refreshPosts(context),
+        child: _buildListPostView(user),
+      ),
+    );
+  }
+
+  PreferredSizeWidget _buildTopAppBar() {
+    return AppBar(
+      title: Row(
+        children: [
+          Image.asset(
+            Assets.images.logoS.path,
+            width: 30,
+            height: 30,
+          ),
+          const SizedBox(width: 6.0),
+          // Hard code: logo (S) + text (Social)
+          const Text(
+            "Social",
+            style: TextStyle(color: Colors.blue),
+          )
+        ],
+      ),
+      automaticallyImplyLeading: false,
+      backgroundColor: Theme.of(context).colorScheme.onPrimary,
+    );
+  }
+
+  Widget _buildHeaderView(UserModel? user) {
     final avatar = user?.avatarUrl;
     final username = user?.username;
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(S.of(context).home),
-        automaticallyImplyLeading: false,
-        backgroundColor: Theme.of(context).colorScheme.onPrimary,
-      ),
-      body: Column(
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Row(
         children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Row(
-              children: [
-                avatar != null
-                    ? CircleAvatar(
-                        radius: 25,
-                        backgroundImage: NetworkImage(
-                          avatar,
-                        ),
-                        onBackgroundImageError: (error, stackTrace) {
-                          // Handle error when loading avatar
-                        },
-                      )
-                    : Container(
-                        width: 50,
-                        height: 50,
-                        clipBehavior: Clip.hardEdge,
-                        decoration: const BoxDecoration(
-                          shape: BoxShape.circle,
-                        ),
-                        child: TextToImage(
-                          text: username.toString()[0],
-                          textSize: 16.0,
-                        ),
-                      ),
-                Expanded(
-                  child: GestureDetector(
-                    onTap: () async {
-                      final newPost = await Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => NewPostScreen()),
-                      );
-                      if (newPost != null) {
-                        context.read<PostCubit>().createPost(newPost);
-                      }
-                    },
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                      child: Container(
-                        padding: const EdgeInsets.all(15.0),
-                        decoration: BoxDecoration(
-                          color: Theme.of(context).colorScheme.surface,
-                          borderRadius: BorderRadius.circular(8),
-                        ),
-                        child: Text(
-                          S.of(context).new_post_box,
-                          style: TextStyle(
-                              color: Theme.of(context).colorScheme.onSurface),
-                        ),
-                      ),
-                    ),
+          avatar != null
+              ? CircleAvatar(
+                  radius: 25,
+                  backgroundImage: NetworkImage(
+                    avatar,
+                  ),
+                  onBackgroundImageError: (error, stackTrace) {
+                    // Handle error when loading avatar
+                  },
+                )
+              : Container(
+                  width: 50,
+                  height: 50,
+                  clipBehavior: Clip.hardEdge,
+                  decoration: const BoxDecoration(
+                    shape: BoxShape.circle,
+                  ),
+                  child: TextToImage(
+                    text: username.toString()[0],
+                    textSize: 16.0,
                   ),
                 ),
-              ],
-            ),
-          ),
-          const Divider(thickness: 0.5),
           Expanded(
-            child: RefreshIndicator(
-              onRefresh: () => _refreshPosts(context),
-              child: BlocBuilder<PostCubit, List<PostModel>>(
-                builder: (context, posts) {
-                  if (posts.isEmpty) {
-                    return const Center(child: Text('No posts available.'));
-                  }
-
-                  final sortedPosts = List<PostModel>.from(posts)
-                    ..sort((a, b) => b.createdAt!.compareTo(a.createdAt!));
-
-                  return ListView.separated(
-                    itemCount: posts.length,
-                    itemBuilder: (context, index) {
-                      final post = sortedPosts[index];
-
-                      return FutureBuilder<UserModel>(
-                        future:
-                            context.read<PostCubit>().getUserById(post.userId!),
-                        builder: (context, snapshot) {
-                          if (snapshot.connectionState ==
-                              ConnectionState.waiting) {
-                            return const Center(
-                                child: CircularProgressIndicator());
-                          } else if (snapshot.hasError) {
-                            return const Center(
-                                child: Text('Error fetching user.'));
-                          } else if (!snapshot.hasData) {
-                            return const Center(
-                              child: Text('User not found.'),
-                            );
-                          }
-
-                          final user = snapshot.data!;
-
-                          return PostWidget(
-                            postData: post,
-                            userData: user,
-                            onTap: () {
-                              // Logic to handle post taps (e.g., navigate to detail screen)
-                            },
-                          );
-                        },
-                      );
-                    },
-                    separatorBuilder: (context, index) =>
-                        const SizedBox(height: 0),
-                  );
-                },
+            child: GestureDetector(
+              onTap: () async {
+                final newPost = await Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => NewPostScreen()),
+                );
+                if (mounted && newPost != null) {
+                  context.read<PostCubit>().createPost(newPost);
+                }
+              },
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                child: Container(
+                  padding: const EdgeInsets.all(15.0),
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).colorScheme.surface,
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  child: Text(
+                    S.of(context).new_post_box,
+                    style: TextStyle(
+                        color: Theme.of(context).colorScheme.onSurface),
+                  ),
+                ),
               ),
             ),
           ),
         ],
       ),
+    );
+  }
+
+  Widget _buildListPostView(UserModel? user) {
+    return BlocBuilder<PostCubit, List<PostModel>>(
+      builder: (context, posts) {
+        if (posts.isEmpty) {
+          return const Center(child: Text('No posts available.'));
+        }
+
+        final sortedPosts = List<PostModel>.from(posts)
+          ..sort((a, b) => b.createdAt!.compareTo(a.createdAt!));
+
+        return ListView.separated(
+          itemCount: posts.length + 1,
+          itemBuilder: (context, index) {
+            if (index == 0) {
+              return _buildHeaderView(user);
+            }
+
+            final post = sortedPosts[index - 1];
+
+            return FutureBuilder<UserModel>(
+              future: context.read<PostCubit>().getUserById(post.userId!),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const Center(child: CircularProgressIndicator());
+                } else if (snapshot.hasError) {
+                  return const Center(child: Text('Error fetching user.'));
+                } else if (!snapshot.hasData) {
+                  return const Center(
+                    child: Text('User not found.'),
+                  );
+                }
+
+                final user = snapshot.data!;
+
+                return PostWidget(
+                  postData: post,
+                  userData: user,
+                  onTap: () {
+                    // Logic to handle post taps (e.g., navigate to detail screen)
+                  },
+                );
+              },
+            );
+          },
+          separatorBuilder: (context, index) {
+            return const Divider(
+              color: Colors.grey,
+              thickness: 0.5,
+            );
+          },
+        );
+      },
     );
   }
 }
--- lib/features/screen/home/view/widget/post_widget.dart
+++ lib/features/screen/home/view/widget/post_widget.dart
@@ -1,21 +1,18 @@
-import 'dart:async';
+import 'dart:ui';
 
-import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter/widgets.dart';
 import 'package:go_router/go_router.dart';
 import 'package:intl/intl.dart';
 import 'package:s_social/core/domain/model/post_model.dart';
 import 'package:s_social/core/domain/model/user_model.dart';
-import 'package:s_social/core/presentation/logic/cubit/app_language/app_language_cubit.dart';
 import 'package:s_social/core/presentation/view/widgets/text_to_image.dart';
 import 'package:s_social/core/utils/app_router/app_router.dart';
-import 'package:s_social/features/screen/home/logic/post_cubit.dart';
 import 'package:s_social/features/screen/home/view/post_screen.dart';
 import 'package:s_social/features/screen/home/view/widget/full_screen_img.dart';
+import 'package:s_social/generated/l10n.dart';
 import 'package:shimmer/shimmer.dart';
-
-import '../../../../../generated/l10n.dart';
 
 class PostWidget extends StatelessWidget {
   final PostModel postData;
@@ -29,16 +26,6 @@ class PostWidget extends StatelessWidget {
     required this.onTap,
   });
 
-  Future<ImageInfo> _getImageInfo(String imageUrl) async {
-    final Completer<ImageInfo> completer = Completer();
-    final ImageStream stream =
-        NetworkImage(imageUrl).resolve(const ImageConfiguration());
-    stream.addListener(
-      ImageStreamListener((ImageInfo info, _) => completer.complete(info)),
-    );
-    return completer.future;
-  }
-
   @override
   Widget build(BuildContext context) {
     String displayName = postData.postAnonymous == true
@@ -48,187 +35,215 @@ class PostWidget extends StatelessWidget {
     String formattedDate =
         DateFormat('dd/MM/yyyy HH:mm').format(postData.createdAt!);
 
-    return Card(
-      margin: const EdgeInsets.all(0.5),
-      shape: const RoundedRectangleBorder(
-          borderRadius: BorderRadius.all(Radius.zero)),
-      child: Column(
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        // Avatar, username, and post time
+        GestureDetector(
+          onTap: () {
+            context.push("${RouterUri.profile}/${userData.id}");
+          },
+          child: _buildPostHeader(
+            context: context,
+            displayName: displayName,
+            datetime: formattedDate,
+          ),
+        ),
+        _buildPostMessage(),
+        _buildPostImage(context: context),
+        _buildPostAction(context: context),
+      ],
+    );
+  }
+
+  Widget _buildPostHeader({
+    required BuildContext context,
+    required String displayName,
+    required String datetime,
+  }) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Row(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          // Avatar, username, and post time
-          GestureDetector(
-            onTap: () {
-              context.push("${RouterUri.profile}/${userData.id}");
-            },
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  userData.avatarUrl != null
-                      ? CircleAvatar(
-                          radius: 20,
-                          backgroundImage: NetworkImage(
-                            userData.avatarUrl!,
-                          ),
-                          onBackgroundImageError: (error, stackTrace) {
-                            // Handle error when loading avatar
-                          },
-                        )
-                      : Container(
-                          width: 40,
-                          height: 40,
-                          clipBehavior: Clip.hardEdge,
-                          decoration: const BoxDecoration(
-                            shape: BoxShape.circle,
-                          ),
-                          child: TextToImage(
-                            text: userData.username.toString()[0],
-                            textSize: 16.0,
-                          ),
-                        ),
-                  const SizedBox(width: 10),
-                  Expanded(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          displayName,
-                          style: const TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 16,
-                          ),
-                        ),
-                        const SizedBox(height: 4),
-                        Text(
-                          formattedDate,
-                          style: TextStyle(
-                            fontSize: 12,
-                            color:
-                                Theme.of(context).colorScheme.onSecondaryFixed,
-                          ),
-                        ),
-                      ],
-                    ),
+          userData.avatarUrl != null
+              ? CircleAvatar(
+                  radius: 20,
+                  backgroundImage: NetworkImage(
+                    userData.avatarUrl!,
                   ),
-                ],
-              ),
-            ),
-          ),
-
-          // Post content
-          if (postData.postContent != null && postData.postContent!.isNotEmpty)
-            Padding(
-              padding:
-                  const EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
-              child: Text(
-                postData.postContent!,
-                style: const TextStyle(fontSize: 14),
-              ),
-            ),
-
-          // Post image (if available)
-          if (postData.postImage != null && postData.postImage!.isNotEmpty)
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 8.0),
-              child: GestureDetector(
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) =>
-                          FullScreenImg(imageUrl: postData.postImage!),
-                    ),
-                  );
-                },
-                child: LayoutBuilder(
-                  builder: (context, constraints) {
-                    return Image.network(
-                      postData.postImage ?? "",
-                      fit: BoxFit.contain,
-                      loadingBuilder: (context, child, loadingProgress) {
-                        if (loadingProgress == null) {
-                          // When the image is fully loaded
-                          return child;
-                        } else {
-                          // Show shimmer background during loading
-                          return Shimmer.fromColors(
-                            baseColor: Colors.grey.shade300,
-                            highlightColor: Colors.grey.shade400,
-                            child: Container(
-                              width: constraints.maxWidth,
-                              height: constraints.maxWidth * 0.6,
-                              color: Colors.white,
-                            ),
-                          );
-                        }
-                      },
-                      errorBuilder: (context, error, stackTrace) {
-                        // Show a fallback UI for errors
-                        return Container(
-                          width: constraints.maxWidth,
-                          height: constraints.maxWidth * 0.6,
-                          color: Colors.grey.shade300,
-                          child: const Icon(Icons.refresh, size: 48),
-                        );
-                      },
-                    );
+                  onBackgroundImageError: (error, stackTrace) {
+                    // Handle error when loading avatar
                   },
+                )
+              : Container(
+                  width: 40,
+                  height: 40,
+                  clipBehavior: Clip.hardEdge,
+                  decoration: const BoxDecoration(
+                    shape: BoxShape.circle,
+                  ),
+                  child: TextToImage(
+                    text: userData.username.toString()[0],
+                    textSize: 16.0,
+                  ),
                 ),
-              ),
-            ),
-
-          // Post actions
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          const SizedBox(width: 10),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Row(
-                  children: [
-                    IconButton(
-                      icon: const Icon(Icons.thumb_up_alt_outlined),
-                      onPressed: () {
-                        // Logic for liking a post
-                      },
-                    ),
-                    Text('${postData.like} ${S.of(context).like}'),
-                  ],
+                Text(
+                  displayName,
+                  style: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 16,
+                  ),
                 ),
-                Row(
-                  children: [
-                    IconButton(
-                      icon: const Icon(Icons.comment_outlined),
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => PostScreen(
-                              postData: postData,
-                              postUserData: userData,
-                            ),
-                          ),
-                        );
-                      },
-                    ),
-                    Text(S.of(context).comment),
-                  ],
-                ),
-                Row(
-                  children: [
-                    IconButton(
-                      icon: const Icon(Icons.share_outlined),
-                      onPressed: () {
-                        // Logic for sharing a post
-                      },
-                    ),
-                    Text(S.of(context).share)
-                  ],
+                const SizedBox(height: 4),
+                Text(
+                  datetime,
+                  style: TextStyle(
+                    fontSize: 12,
+                    color: Theme.of(context).colorScheme.onSecondaryFixed,
+                  ),
                 ),
               ],
             ),
           ),
         ],
+      ),
+    );
+  }
+
+  Widget _buildPostMessage() {
+    if (postData.postContent != null && postData.postContent!.isNotEmpty) {
+      return Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
+        child: Text(
+          postData.postContent!,
+          style: const TextStyle(fontSize: 14),
+        ),
+      );
+    }
+
+    return const SizedBox.shrink();
+  }
+
+  Widget _buildPostImage({
+    required BuildContext context,
+  }) {
+    if (postData.postImage != null && postData.postImage!.isNotEmpty) {
+      return Padding(
+        padding: const EdgeInsets.symmetric(vertical: 8.0),
+        child: GestureDetector(
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) =>
+                    FullScreenImg(imageUrl: postData.postImage!),
+              ),
+            );
+          },
+          child: LayoutBuilder(
+            builder: (context, constraints) {
+              return Image.network(
+                postData.postImage ?? "",
+                fit: BoxFit.contain,
+                loadingBuilder: (context, child, loadingProgress) {
+                  if (loadingProgress == null) {
+                    // When the image is fully loaded
+                    return child;
+                  } else {
+                    // Show shimmer background during loading
+                    return Shimmer.fromColors(
+                      baseColor: Colors.grey.shade300,
+                      highlightColor: Colors.grey.shade400,
+                      child: Container(
+                        width: constraints.maxWidth,
+                        height: constraints.maxWidth * 0.6,
+                        color: Colors.white,
+                      ),
+                    );
+                  }
+                },
+                errorBuilder: (context, error, stackTrace) {
+                  // Show a fallback UI for errors
+                  return Container(
+                    width: constraints.maxWidth,
+                    height: constraints.maxWidth * 0.6,
+                    color: Colors.grey.shade300,
+                    child: const Icon(Icons.refresh, size: 48),
+                  );
+                },
+              );
+            },
+          ),
+        ),
+      );
+    }
+
+    return const SizedBox.shrink();
+  }
+
+  Widget _buildPostAction({
+    required BuildContext context,
+  }) {
+    return Row(
+      mainAxisSize: MainAxisSize.max,
+      children: [
+        _buildPostItem(
+          icon: Icons.thumb_up_alt_outlined,
+          label: "${postData.like} ${S.of(context).like}",
+          onTap: () {},
+        ),
+        _buildPostItem(
+          icon: Icons.comment_outlined,
+          label: S.of(context).comment,
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => PostScreen(
+                  postData: postData,
+                  postUserData: userData,
+                ),
+              ),
+            );
+          },
+        ),
+        _buildPostItem(
+          icon: Icons.share_outlined,
+          label: S.of(context).share,
+          onTap: () {},
+        ),
+      ],
+    );
+  }
+
+  Widget _buildPostItem({
+    required IconData icon,
+    required String label,
+    required VoidCallback onTap,
+  }) {
+    return Expanded(
+      flex: 1,
+      child: InkWell(
+        onTap: onTap,
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(icon),
+                const SizedBox(width: 6.0),
+                Text(label),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
