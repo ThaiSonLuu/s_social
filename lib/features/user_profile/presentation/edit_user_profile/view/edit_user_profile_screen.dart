23cd192a16fd1f645701a5cfb560d473f8fd76f0
ThaiSonLuu <sanryoo6@gmail.com>
2024-11-30 21:25:36 +0700

    Shimmer loading and cache image for edit user profile

--- lib/features/user_profile/presentation/edit_user_profile/view/edit_user_profile_screen.dart
+++ lib/features/user_profile/presentation/edit_user_profile/view/edit_user_profile_screen.dart
@@ -8,7 +8,9 @@ import 'package:image_picker/image_picker.dart';
 import 'package:s_social/core/domain/model/user_model.dart';
 import 'package:s_social/core/presentation/view/widgets/text_field.dart';
 import 'package:s_social/core/presentation/view/widgets/text_to_image.dart';
+import 'package:s_social/core/utils/shimmer_loading.dart';
 import 'package:s_social/core/utils/snack_bar.dart';
+import 'package:s_social/core/utils/ui/cache_image.dart';
 import 'package:s_social/di/injection_container.dart';
 import 'package:s_social/features/user_profile/presentation/edit_user_profile/logic/edit_user_profile_cubit.dart';
 import 'package:s_social/gen/assets.gen.dart';
@@ -112,6 +114,10 @@ class _UserProfileScreenState extends State<_UserProfileScreen> {
         }
       },
       builder: (context, state) {
+        if (state is EditUserProfileLoading) {
+          return const EditUserProfileShimmer();
+        }
+
         String? avatarUrl;
         String? backgroundUrl;
         String? username;
@@ -165,6 +171,7 @@ class _UserProfileScreenState extends State<_UserProfileScreen> {
                 ),
               ],
             ),
+            const SizedBox(height: 24),
             _buildNameView(
               context: context,
               username: username,
@@ -190,66 +197,28 @@ class _UserProfileScreenState extends State<_UserProfileScreen> {
         aspectRatio: 2.2,
         child: Stack(
           children: [
-            isLoading
-                ? Container(
+            _selectedBackground != null
+                ? SizedBox(
                     width: double.maxFinite,
                     height: double.maxFinite,
-                    color: Theme.of(context).colorScheme.surfaceContainer,
+                    child: Image.file(
+                      _selectedBackground!,
+                      fit: BoxFit.cover,
+                    ),
                   )
-                : (_selectedBackground != null
+                : ((backgroundUrl ?? "").isNotEmpty
                     ? SizedBox(
                         width: double.maxFinite,
                         height: double.maxFinite,
-                        child: Image.file(
-                          _selectedBackground!,
-                          fit: BoxFit.cover,
-                          errorBuilder: (context, error, stackTrace) {
-                            return SizedBox(
-                              width: 110,
-                              height: 110,
-                              child: Image.asset(
-                                randomBackground.path,
-                                fit: BoxFit.fill,
-                              ),
-                            );
-                          },
-                          frameBuilder:
-                              (context, child, frame, wasSynchronouslyLoaded) {
-                            return child;
-                          },
+                        child: CacheImage(
+                          imageUrl: backgroundUrl ?? "",
+                          loadingWidth: double.maxFinite,
+                          loadingHeight: double.maxFinite,
                         ),
                       )
-                    : SizedBox(
-                        width: double.maxFinite,
-                        height: double.maxFinite,
-                        child: Image.network(
-                          backgroundUrl ?? "",
-                          fit: BoxFit.cover,
-                          loadingBuilder: (context, child, loadingProgress) {
-                            return Container(
-                              width: 110,
-                              height: 110,
-                              color: Theme.of(context)
-                                  .colorScheme
-                                  .surfaceContainer,
-                              child: child,
-                            );
-                          },
-                          errorBuilder: (context, error, stackTrace) {
-                            return SizedBox(
-                              width: 110,
-                              height: 110,
-                              child: Image.asset(
-                                randomBackground.path,
-                                fit: BoxFit.fill,
-                              ),
-                            );
-                          },
-                          frameBuilder:
-                              (context, child, frame, wasSynchronouslyLoaded) {
-                            return child;
-                          },
-                        ),
+                    : Image.asset(
+                        randomBackground.path,
+                        fit: BoxFit.fill,
                       )),
             Align(
               alignment: Alignment.center,
@@ -313,61 +282,22 @@ class _UserProfileScreenState extends State<_UserProfileScreen> {
             decoration: const BoxDecoration(
               shape: BoxShape.circle,
             ),
-            child: isLoading
-                ? Container(
-                    width: 110,
-                    height: 110,
-                    color: Theme.of(context).colorScheme.surfaceContainer,
+            child: _selectedAvatar != null
+                ? Image.file(
+                    _selectedAvatar!,
+                    fit: BoxFit.cover,
+                    width: 120,
+                    height: 120,
                   )
-                : (_selectedAvatar != null
-                    ? Image.file(
-                        _selectedAvatar!,
-                        fit: BoxFit.cover,
-                        width: 110,
-                        height: 110,
-                        errorBuilder: (context, error, stackTrace) {
-                          return SizedBox(
-                            width: 110,
-                            height: 110,
-                            child: TextToImage(
-                              text: username?[0].toUpperCase() ?? "S",
-                              textSize: 48,
-                            ),
-                          );
-                        },
-                        frameBuilder:
-                            (context, child, frame, wasSynchronouslyLoaded) {
-                          return child;
-                        },
+                : ((avatarUrl ?? "").isNotEmpty
+                    ? CacheImage(
+                        imageUrl: avatarUrl ?? "",
+                        loadingWidth: 120,
+                        loadingHeight: 120,
                       )
-                    : Image.network(
-                        avatarUrl ?? "",
-                        fit: BoxFit.cover,
-                        width: 110,
-                        height: 110,
-                        loadingBuilder: (context, child, loadingProgress) {
-                          return Container(
-                            width: 110,
-                            height: 110,
-                            color:
-                                Theme.of(context).colorScheme.surfaceContainer,
-                            child: child,
-                          );
-                        },
-                        errorBuilder: (context, error, stackTrace) {
-                          return SizedBox(
-                            width: 110,
-                            height: 110,
-                            child: TextToImage(
-                              text: username?[0].toUpperCase() ?? "S",
-                              textSize: 48,
-                            ),
-                          );
-                        },
-                        frameBuilder:
-                            (context, child, frame, wasSynchronouslyLoaded) {
-                          return child;
-                        },
+                    : TextToImage(
+                        text: username?[0].toUpperCase() ?? "S",
+                        textSize: 48,
                       )),
           ),
           Align(
@@ -489,7 +419,7 @@ class _UserProfileScreenState extends State<_UserProfileScreen> {
                   controller: _nameCtrl,
                   labelText: S.of(context).showing_name,
                 ),
-          const SizedBox(height: 24.0),
+          const SizedBox(height: 14.0),
           email == null
               ? Container(
                   width: 220,
@@ -505,7 +435,7 @@ class _UserProfileScreenState extends State<_UserProfileScreen> {
                   enable: false,
                 ),
           if (bio != null) ...[
-            const SizedBox(height: 24.0),
+            const SizedBox(height: 14.0),
             STextField(
               controller: _bioCtrl,
               labelText: S.of(context).bio,
@@ -514,6 +444,144 @@ class _UserProfileScreenState extends State<_UserProfileScreen> {
             ),
           ],
         ],
+      ),
+    );
+  }
+}
+
+class EditUserProfileShimmer extends StatelessWidget {
+  const EditUserProfileShimmer({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Stack(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(bottom: 30.0),
+              child: _buildBackgroundImage(),
+            ),
+            Positioned(
+              top: 12.0,
+              left: 12.0,
+              child: _buildBackIcon(context),
+            ),
+            Positioned(
+              top: 12.0,
+              right: 12.0,
+              child: _buildSaveButton(context),
+            ),
+            Positioned(
+              left: 20.0,
+              bottom: 0.0,
+              child: _buildAvatarImage(context),
+            ),
+          ],
+        ),
+        const SizedBox(height: 24),
+        const Padding(
+          padding: EdgeInsets.symmetric(horizontal: 16.0),
+          child: ShimmerLoading(
+            width: double.maxFinite,
+            height: 60,
+            borderRadius: 12,
+          ),
+        ),
+        const SizedBox(height: 14),
+        const Padding(
+          padding: EdgeInsets.symmetric(horizontal: 16.0),
+          child: ShimmerLoading(
+            width: double.maxFinite,
+            height: 60,
+            borderRadius: 12,
+          ),
+        ),
+        const SizedBox(height: 14),
+        const Padding(
+          padding: EdgeInsets.symmetric(horizontal: 16.0),
+          child: ShimmerLoading(
+            width: double.maxFinite,
+            height: 150,
+            borderRadius: 12,
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildBackgroundImage() {
+    return const SizedBox(
+      width: double.maxFinite,
+      child: AspectRatio(
+        aspectRatio: 2.2,
+        child: ShimmerLoading(
+          width: double.maxFinite,
+          height: double.maxFinite,
+        ),
+      ),
+    );
+  }
+
+  Widget _buildAvatarImage(BuildContext context) {
+    return Container(
+      clipBehavior: Clip.hardEdge,
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        color: Theme.of(context).colorScheme.surface,
+      ),
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        clipBehavior: Clip.hardEdge,
+        decoration: const BoxDecoration(
+          shape: BoxShape.circle,
+        ),
+        child: const ShimmerLoading(
+          width: 110,
+          height: 110,
+        ),
+      ),
+    );
+  }
+
+  Widget _buildBackIcon(BuildContext context) {
+    return InkWell(
+      onTap: () {
+        context.pop();
+      },
+      child: Container(
+        clipBehavior: Clip.hardEdge,
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          color:
+              Theme.of(context).colorScheme.surfaceContainer.withOpacity(0.6),
+        ),
+        child: const Padding(
+          padding: EdgeInsets.all(8.0),
+          child: Icon(Icons.arrow_back_ios_new_rounded),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildSaveButton(BuildContext context) {
+    return InkWell(
+      onTap: () {
+        context.pop();
+      },
+      child: Container(
+        clipBehavior: Clip.hardEdge,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(8.0),
+          color:
+              Theme.of(context).colorScheme.surfaceContainer.withOpacity(0.6),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Text(S.of(context).save),
+        ),
       ),
     );
   }
